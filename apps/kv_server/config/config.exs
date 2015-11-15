4ac7b803587f5cd38db2703b7952c508c24fd4b3
Ioan Dobricean <ioan.dobricean@gmail.com>
2015-11-15 21:50:10 +0200

    Move kv_server port to config

--- apps/kv_server/config/config.exs
+++ apps/kv_server/config/config.exs
@@ -20,6 +20,7 @@ use Mix.Config
 #
 #     config :logger, level: :info
 #
+config :kv_server, port: 4040
 
 # It is also possible to import configuration files, relative to this
 # directory. For example, you can emulate configuration per environment
